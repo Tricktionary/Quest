3f630ffce4612b4459d5b9847959805a570e318f
Andy <andyaoux@hotmail.com>
2018-01-21 17:00:16 -0500

    Removed issue with buggy script component

--- QUEST/Assets/script/MainMenu.cs
+++ QUEST/Assets/script/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	public void PlayGame(){
+	public void PlayGame(){ //Loads The "PlayField"
 		SceneManager.LoadScene("playField");
 	}
 }
