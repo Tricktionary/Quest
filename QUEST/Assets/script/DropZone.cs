28007db51ed4378273ff01e73ffb3a5751827683
Andy <andyaoux@hotmail.com>
2018-01-21 12:31:03 -0500

    Drag and Drop Functionality works

---
+++ QUEST/Assets/script/DropZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public void OnPointerEnter(PointerEventData eventData){
+
+	}
+	public void OnPointerExit(PointerEventData eventData){
+		
+	}
+	public void OnDrop(PointerEventData eventData){
+		Debug.Log(eventData.pointerDrag.name + " Drop to "+ gameObject.name);
+
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if(d!=null){
+			d.parentToReturnTo = this.transform;
+		}
+	}
+}
