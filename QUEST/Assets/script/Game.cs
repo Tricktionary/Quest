a4779cbb6fa6297b38f4cae4d56f311eb19a519a
Andy <andyaoux@hotmail.com>
2018-01-23 12:14:12 -0500

    Implementation start

--- QUEST/Assets/script/Game.cs
+++ QUEST/Assets/script/Game.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
-	public CardArea area;
+
+	
+	private List<Player> _players = new List<Player>(); 	//List of players
+	private int _turnId; 									//Player ID of who's turn
+	private Deck _adventureDeck;
+	private Deck _storyDeck;
+
+	//Draws Card 
+	void DrawCard(){
+	}
+
+	//End Turn
+	void EndTurn(){
+	}
 
 	// Use this for initialization
 	void Start () {
