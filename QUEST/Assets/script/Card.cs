b5c3b045c17f5f5fa46b911a0493691417e1ff53
Andy <andyaoux@hotmail.com>
2018-02-03 10:30:31 -0500

    Moved drag and drop to Adventure cards only

--- QUEST/Assets/script/Card.cs
+++ QUEST/Assets/script/Card.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
  
-public class Card : MonoBehaviour,IBeginDragHandler,IDragHandler, IEndDragHandler,IDropHandler{
+public class Card : MonoBehaviour{
+
+	public Transform oldPosition = null; 	//Old Position of the card on the board
 
 	public string _name {
 		get;
@@ -14,42 +16,6 @@ public class Card : MonoBehaviour,IBeginDragHandler,IDragHandler, IEndDragHandle
 		get;
 		set;
 	}
-
-	public Transform oldPosition = null; 	//Old Position of the card on the board
-	
-	//Action that occurs when you begin to drag
-	public void OnBeginDrag(PointerEventData eventData){
-		Debug.Log("OnBeginDrag");
-		oldPosition = this.transform.parent;
-		this.transform.SetParent(this.transform.parent.parent);
-		GetComponent<CanvasGroup>().blocksRaycasts = false;
-		CardArea[] zone = GameObject.FindObjectsOfType<CardArea>(); //Find Drop Zones
-	}
-
-	//Action beginnning onDrag
-	public void OnDrag(PointerEventData eventData){
-		//Debug.Log("Drag");
-		this.transform.position = eventData.position;
-
-	}
-
-	//Action that occurs at the end of the drag
-	public void OnEndDrag(PointerEventData eventData){
-		Debug.Log("OnEndDrag");
-		this.transform.SetParent(oldPosition);
-		GetComponent<CanvasGroup>().blocksRaycasts = true;
-	}
-
-	//When a card is dropped
-	public void OnDrop(PointerEventData eventData){
-		Debug.Log(eventData.pointerDrag.name + " Drop to "+ gameObject.name);
-		Card d = eventData.pointerDrag.GetComponent<Card>();
-
-		if(d!=null){
-			//_area.Add (d);
-			//Debug.Log (_area[0].name);
-			d.oldPosition = this.transform;
-		}
-
-	}
+		
 }
+
