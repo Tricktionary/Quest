11e1eda3b9a4a55cc6467ddf508e9787b52b614e
joshua-arts <josha5@outlook.com>
2018-02-22 18:40:44 -0500

    clean Deck.cs

--- QUEST/Assets/script/Deck.cs
+++ QUEST/Assets/script/Deck.cs
@@ -6,14 +6,16 @@ public class Deck{
 
 	// The list of cards that make up the deck.
 	private List<Card> _cards = new List<Card>();
-	
+
+	// Getter for the deck cards.
 	public List<Card> GetDeck(){
 		return _cards;
 	}
-	//Constructor. 
+
+	// Deck constructor.
 	public Deck(string type){
 		if (type.Equals ("Adventure")) {
-			//Fill deck with adventure cards.
+			// Create the adventure deck.
 
 			/* Weapon Load */
 			/* 2 Excaliburs */
@@ -35,26 +37,25 @@ public class Deck{
 			addWeapon("Dagger", 5, "card_image/weapons/weaponCard2", 6);
 
 			/* Foe Load */
-
 			/* 7 Robber Knight */
 			addFoe("Robber Knight","Robber Knight", 15, 15, false, "card_image/foe/foeCard1", 7);
 
 			/* 5 Saxons */
 			addFoe("Saxons","Saxon", 10, 20, false, "card_image/foe/foeCard2", 5);
 
-			/* 4 Boar*/
+			/* 4 Boar */
 			addFoe("Boar","Boar", 5, 15, false, "card_image/foe/foeCard3", 4);
 
-			/* 8 Thieves*/
+			/* 8 Thieves */
 			addFoe("Thieves","Thieves", 5, 5, false, "card_image/foe/foeCard4", 8);
 
-			/* 2 Green Knight*/
+			/* 2 Green Knight */
 			addFoe("Green Knight","Green Knight", 25, 40, false, "card_image/foe/foeCard5", 2);
 
 			/* 3 Black Knight */
 			addFoe("Black Knight","Black Knight", 25, 35, false, "card_image/foe/foeCard6", 3);
 
-			/* 6 Evil Knight*/
+			/* 6 Evil Knight */
 			addFoe("Evil Knight","Evil Knight", 20, 30, false, "card_image/foe/foeCard7", 6);
 
 			/* 8 Saxon Knight */
@@ -70,7 +71,6 @@ public class Deck{
 			addFoe("Mordred","Mordred", 30, 30, true, "card_image/foe/foeCard11", 4);
 
 			/* Ally load */
-
 			/* Sir Pellinore */
 			addAlly("Sir Pellinore",10,0,0,4,"Search for the Questing Beast", null, false, "card_image/special/specialCard1", 1);
 
@@ -102,81 +102,99 @@ public class Deck{
 			addAlly("Sir Gawain", 10, 0, 20, 0, "Test of the Green Knight", null, false, "card_image/special/specialCard11", 1);        
 
 			/* Amour load */
-			// NOTE: Make an addArmour method for loading these.
-			addArmour("Amour",10,1, "card_image/special/specialCard3",8);
-
-			//Test
-			/*
-			addTestCard("Test of the Questing Beast",4,"card_image/tests/testCard",10);
-			addTestCard("Test of Temptation",0,"card_image/tests/testCard1",10);
-			addTestCard("Test of Valor",0,"card_image/tests/testCard2",2);
-			addTestCard("Test of the Morgan Le Fey",4,"card_image/tests/testCard3",2);
-			*/
-
-			// Shuffe the deck of cards after adding.
-			this.Shuffle();
+			addArmour("Amour", 10, 1, "card_image/special/specialCard3", 8);
 		}else if (type.Equals ("QuestOnly")) {
-			addQuest("Search for the Holy Grail", 5, "*", "card_image/quest/questCard9", 1);
-			addQuest("Test of the Green Knight", 4, "Green Knight", "card_image/quest/questCard10", 1);
-			addQuest("Search for the Questing Beast", 4, "", "card_image/quest/questCard5", 1);
-			addQuest("Defend the Queen's Honor", 4, "*", "card_image/quest/questCard6", 1);
-			addQuest("Rescue the Fair Maiden", 3, "Black Knight", "card_image/quest/questCard8", 1);
-			addQuest("Journey Through the Enchanted Forest", 3, "Evil Knight", "card_image/quest/questCard1", 1);
-			addQuest("Vanquish King Arthur's Enemies", 3, "", "card_image/quest/questCard2", 2);
-			addQuest("Slay the Dragon", 3, "Dragon", "card_image/quest/questCard7", 1);
-			addQuest("Boar Hunt", 2, "Boar", "card_image/quest/questCard4", 2);
-			addQuest("Repel the Saxxon Raiders", 2, "Saxon", "card_image/quest/questCard3", 2);
+			addQuestCards();
 		}
 		else if (type.Equals ("TournamentOnly")) {
-			addTournament("Tournament at Camelot",3,"card_image/tournament/TournamentCard",10);
-			addTournament("Tournament at Orkney",2,"card_image/tournament/TournamentCard1",10);
-			addTournament("Tournament at Tintagel",1,"card_image/tournament/TournamentCard2",10);
-			addTournament("Tournament at York",0,"card_image/tournament/TournamentCard3",10);
+			addTournamentCards();
 		}
 		else if (type.Equals ("EventOnly")) {
-			addEvent("Chivalrous Deed", "lowest rank and shield receives 3 shields", "card_image/events/eventCard1", 1); //working/tested
-			addEvent("Pox", "All players except player drawing this card lose 1 shield", "card_image/events/eventCard2", 1); //working/tested
-			addEvent("Plague", "Drawer loses 2 shields if possible", "card_image/events/eventCard3", 1); //working/tested
-			addEvent("King's Recognition", "The next player(s) to complete a quest will receive 2 extra shields", "card_image/events/eventCard4", 2); //working/tested
-			//addEvent("Queen's Favor", "The lowest ranked player(s) immediately receives 2 Adventure cards", "card_image/events/eventCard5", 2);
-			//addEvent("Court Called to Camelot", "All Allies in play must be discarded", "card_image/events/eventCard6", 2);
-			//addEvent("King's Call to Arms", "Highest ranked player(s) must discard 1 weapon, if unable 2 foe cards must be discarded", "card_image/events/eventCard7", 1);
-			addEvent("Prosperity Throughout the Realm", "All players may immediately draw 2 adventure Cards", "card_image/events/eventCard8", 1);
+			addEventCards();
 		}
 		else if (type.Equals ("Story")) {
-			//WHEN ADDING FEATURED FOE * means all
 			// Fill the deck with story cards.
-			
-			addQuest("Search for the Holy Grail", 5, "*", "card_image/quest/questCard9", 1);
-			addQuest("Test of the Green Knight", 4, "Green Knight", "card_image/quest/questCard10", 1);
-			addQuest("Search for the Questing Beast", 4, "", "card_image/quest/questCard5", 1);
-			addQuest("Defend the Queen's Honor", 4, "*", "card_image/quest/questCard6", 1);
-			addQuest("Rescue the Fair Maiden", 3, "Black Knight", "card_image/quest/questCard8", 1);
-			addQuest("Journey Through the Enchanted Forest", 3, "Evil Knight", "card_image/quest/questCard1", 1);
-			addQuest("Vanquish King Arthur's Enemies", 3, "", "card_image/quest/questCard2", 2);
-			addQuest("Slay the Dragon", 3, "Dragon", "card_image/quest/questCard7", 1);
-			addQuest("Boar Hunt", 2, "Boar", "card_image/quest/questCard4", 2);
-			addQuest("Repel the Saxxon Raiders", 2, "Saxon", "card_image/quest/questCard3", 2);
-			
-			//Tournemant
-			addTournament("Tournament at Camelot",3,"card_image/tournament/TournamentCard",1);
-			addTournament("Tournament at Orkney",2,"card_image/tournament/TournamentCard1",1);
-			addTournament("Tournament at Tintagel",1,"card_image/tournament/TournamentCard2",1);
-			addTournament("Tournament at York",0,"card_image/tournament/TournamentCard3",1);
-			
-			//add event Cards 
-			addEvent("Chivalrous Deed", "lowest rank and shield receives 3 shields", "card_image/events/eventCard1", 1); //working/tested
-			addEvent("Pox", "All players except player drawing this card lose 1 shield", "card_image/events/eventCard2", 1); //working/tested
-			addEvent("Plague", "Drawer loses 2 shields if possible", "card_image/events/eventCard3", 1); //working/tested
-			addEvent("King's Recognition", "The next player(s) to complete a quest will receive 2 extra shields", "card_image/events/eventCard4", 2); //working/tested
-			//addEvent("Queen's Favor", "The lowest ranked player(s) immediately receives 2 Adventure cards", "card_image/events/eventCard5", 2);
-			//addEvent("Court Called to Camelot", "All Allies in play must be discarded", "card_image/events/eventCard6", 2);
-			//addEvent("King's Call to Arms", "Highest ranked player(s) must discard 1 weapon, if unable 2 foe cards must be discarded", "card_image/events/eventCard7", 1);
-			addEvent("Prosperity Throughout the Realm", "All players may immediately draw 2 adventure Cards", "card_image/events/eventCard8", 1);
+			addQuestCards();
+			addTournamentCards();
+			addEventCards();
 		}
 
 		// Shuffe the deck of cards after adding.
 		this.Shuffle();
+	}
+
+	void addQuestCards(){
+		/* 1 Search for the Holy Grail */
+		addQuest("Search for the Holy Grail", 5, "*", "card_image/quest/questCard9", 1);
+
+		/* 1 Test of the Green Knight */
+		addQuest("Test of the Green Knight", 4, "Green Knight", "card_image/quest/questCard10", 1);
+
+		/* 1 Search for the Questing Beast */
+		addQuest("Search for the Questing Beast", 4, "", "card_image/quest/questCard5", 1);
+
+		/* 1 Defend the Queen's Honor */
+		addQuest("Defend the Queen's Honor", 4, "*", "card_image/quest/questCard6", 1);
+
+		/* 1 Rescue the Fair Maiden */
+		addQuest("Rescue the Fair Maiden", 3, "Black Knight", "card_image/quest/questCard8", 1);
+
+		/* 1 Journey Through the Enchanted Forest */
+		addQuest("Journey Through the Enchanted Forest", 3, "Evil Knight", "card_image/quest/questCard1", 1);
+
+		/* 2 Vanquish King Arthur's Enemies */
+		addQuest("Vanquish King Arthur's Enemies", 3, "", "card_image/quest/questCard2", 2);
+
+		/* 1 Slay the Dragon */
+		addQuest("Slay the Dragon", 3, "Dragon", "card_image/quest/questCard7", 1);
+
+		/* 2 Boar Hunt */
+		addQuest("Boar Hunt", 2, "Boar", "card_image/quest/questCard4", 2);
+
+		/* 2 Repel the Saxxon Raiders */
+		addQuest("Repel the Saxxon Raiders", 2, "Saxon", "card_image/quest/questCard3", 2);
+	}
+
+	void addTournamentCards(){
+		/* 1 Tournament at Camelot */
+		addTournament("Tournament at Camelot", 3, "card_image/tournament/TournamentCard", 1);
+
+		/* 1 Tournament at Orkney */
+		addTournament("Tournament at Orkney", 2, "card_image/tournament/TournamentCard1", 1);
+
+		/* 1 Tournamnt at Tintagel */
+		addTournament("Tournament at Tintagel", 1, "card_image/tournament/TournamentCard2", 1);
+
+		/* 1 Tournament at York */
+		addTournament("Tournament at York", 0, "card_image/tournament/TournamentCard3", 1);
+	}
+
+	// NOTE: The following events are working and tested.
+	// Chivalrous Deed, Pox, Plague, King's Recognition
+	void addEventCards(){
+		/* 1 Chivalrous Deed */
+		addEvent("Chivalrous Deed", "lowest rank and shield receives 3 shields", "card_image/events/eventCard1", 1);
+
+		/* 1 Pox */
+		addEvent("Pox", "All players except player drawing this card lose 1 shield", "card_image/events/eventCard2", 1);
+
+		/* 1 Plague */
+		addEvent("Plague", "Drawer loses 2 shields if possible", "card_image/events/eventCard3", 1);
+
+		/* 2 King's Recognition */
+		addEvent("King's Recognition", "The next player(s) to complete a quest will receive 2 extra shields", "card_image/events/eventCard4", 2);
+
+		/* 2 Queen's Favor */
+		//addEvent("Queen's Favor", "The lowest ranked player(s) immediately receives 2 Adventure cards", "card_image/events/eventCard5", 2);
+
+		/* 2 Court Called to Camelot */
+		//addEvent("Court Called to Camelot", "All Allies in play must be discarded", "card_image/events/eventCard6", 2);
+
+		/* 1 King's Call to Arms */
+		//addEvent("King's Call to Arms", "Highest ranked player(s) must discard 1 weapon, if unable 2 foe cards must be discarded", "card_image/events/eventCard7", 1);
+
+		/* 1 Properity Throughout the Realm */
+		addEvent("Prosperity Throughout the Realm", "All players may immediately draw 2 adventure Cards", "card_image/events/eventCard8", 1);
 	}
 
 	void addTestCard(string name, int minBid, string asset, int copies){
