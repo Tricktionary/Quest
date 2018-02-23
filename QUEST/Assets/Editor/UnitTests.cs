6bc46fdb82441ddc8e058120eb06128c7559f74a
Andy <andyaoux@hotmail.com>
2018-02-22 22:56:10 -0500

    Unit testing move files

--- QUEST/Assets/Editor/UnitTests.cs
+++ QUEST/Assets/Editor/UnitTests.cs
@@ -6,12 +6,13 @@ using System.Collections;
 
 public class UnitTests {
 
-	int currentStoryCardTotal = 13;
-	int currentAdventureCardTotal = 99;
+	 
 
 	[Test]
 	//This test have a small chance to fail. This is because it is testing shuffle which means in a very small chance it will shuffle unevenly
 	public void Test_Deck() {
+		int currentStoryCardTotal = 23;			//Total Cards
+		int currentAdventureCardTotal = 117;	//Total Cards
 		//test story init, then adventure init then blank (discard) init
 		Deck deck = new Deck ("Story");
 		//test draw
@@ -45,30 +46,85 @@ public class UnitTests {
 
 	[Test]
 	public void Test_Player() {
-		
+		Player player1 = new Player(1);
+		Player player2 = new Player(2);
+		Deck deck = new Deck ("Adventure");
+
+		//Test Uniqueness of player
+		Assert.AreEqual(player1.playerId,1);
+		Assert.AreEqual(player2.playerId,2);
+		Assert.AreNotEqual(player1.playerId,player2.playerId);
+
+		for(int i = 0 ; i < 12 ; i++){
+			player1.addCard(deck.Draw());
+			player2.addCard(deck.Draw());
+		}
+		Assert.AreEqual(player1.hand.Count , 12 );
+		Assert.AreEqual(player2.hand.Count , 12 );
 	}
 
 	[Test]
 	public void Test_FoeCard() {
+		//public FoeCard(string name, string type,  int loPower, int hiPower, bool special, string asset) {
+
+		FoeCard foeCard = new FoeCard("FoeCard","Foe",0,10,false,"/GG.png");
+
+		Assert.AreEqual(foeCard.name,"FoeCard");
+		Assert.AreEqual(foeCard.type,"Foe");
+		Assert.AreEqual(foeCard.loPower,0);
+		Assert.AreEqual(foeCard.hiPower,10);
+		Assert.AreEqual(foeCard.special,false);
+		Assert.AreEqual(foeCard.asset,"/GG.png");
 
 	}
 	[Test]
 	public void Test_QuestCard() {
+		//public QuestCard (string name, int stages, string featuredFoe, string asset) {
+
+		QuestCard questCard = new QuestCard("Quest",5,"Jim","gg.png");
+
+		Assert.AreEqual(questCard.name,"Quest");
+		Assert.AreEqual(questCard.stages, 5);
+		Assert.AreEqual(questCard.featuredFoe, "Jim");
+		Assert.AreEqual(questCard.asset,"gg.png");
 
 	}
 	[Test]
 	public void Test_TestCard() {
+		//public TestCard(string name, int minBids, string asset) {
 
+		TestCard testCard = new TestCard("Test",20,"img.png");
+
+		Assert.AreEqual(testCard.name,"Test");
+		Assert.AreEqual(testCard.minBids,20);
+		Assert.AreEqual(testCard.asset,"img.png");
 	}
 	[Test]
 	public void Test_TournamentCard() {
-
+		//public TournamentCard(string name, int shields, string asset) {
+		TournamentCard tourneyCard = new TournamentCard("Tourney",10,"Test.png");
+		
+		Assert.AreEqual(tourneyCard.name,"Tourney");
+		Assert.AreEqual(tourneyCard.shields,10);
+		Assert.AreEqual(tourneyCard.asset,"Test.png");
 	}
 	[Test]
 	public void Test_WeaponCard() {
+		//public WeaponCard(string name, int power, string asset) {
 
+		WeaponCard weaponCard = new WeaponCard("Sword",100,"test.png");
+
+		Assert.AreEqual(weaponCard.name,"Sword");
+		Assert.AreEqual(weaponCard.power,100);
+		Assert.AreEqual(weaponCard.asset,"test.png");
 	}
 
+	[Test]
+	public void Test_Game(){
+
+
+
+	}
 	// A UnityTest behaves like a coroutine in PlayMode
 	// and allows you to yield null to skip a frame in EditMode
 	[UnityTest]
