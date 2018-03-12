5893d2171e74618d3b86935be0639e86145b7c8c
Andy <andyaoux@hotmail.com>
2018-03-12 16:17:01 -0400

    Networking GUI

---
+++ Iteration2/QUEST/Assets/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+	public GameObject MenuPanel;
+	public GameObject Network;
+	public GameObject HotSeatGameMode;
+
+	public void HotSeatMode(){
+		MenuPanel.SetActive(false);
+		HotSeatGameMode.SetActive(true);
+	}
+
+	public void MultiplayerMode(){
+		MenuPanel.SetActive(false);
+		Network.SetActive(true);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+}
