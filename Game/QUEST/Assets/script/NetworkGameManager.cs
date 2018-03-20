15d2894f3721646b3b5bbe6fddfa4ae4753881f9
Andy <andyaoux@hotmail.com>
2018-03-20 13:22:40 -0400

    PlayerObject

--- Game/QUEST/Assets/script/NetworkGameManager.cs
+++ Game/QUEST/Assets/script/NetworkGameManager.cs
@@ -39,7 +39,7 @@ public class NetworkGameManager : NetworkBehaviour
 	public GameObject discardPile;
 	public GameObject rankCardArea;
 	public GameObject drawCardArea;
-	public GameObject Hand;
+	//public GameObject Hand;
 	public GameObject winScreen;
 	public GameObject winScreenTxt;
 
@@ -79,60 +79,7 @@ public class NetworkGameManager : NetworkBehaviour
     return currentNewId;
   }
 
-  // Load a player.
-  public void loadPlayer(int n){
-//    logger.info("Loading player: " + (n + 1));
-    foreach (Transform child in Hand.transform) {
-      GameObject.Destroy(child.gameObject);
-    }
-
-    foreach (Transform child in playArea.transform) {
-      GameObject.Destroy(child.gameObject);
-    }
-
-    // Set Player ID text.
-    playerIdTxt.GetComponent<UnityEngine.UI.Text>().text = "Player ID: " + (n + 1);
-
-    // Get current players shield.
-    shieldCounterTxt.GetComponent<UnityEngine.UI.Text>().text = "# Shield: "+ (_players[n].shieldCounter);
-
-    // Load their hand.
-    loadHand(n);
-  }
-
-  // Load the players hand onto the UI.
-  void loadHand(int playerId){
-    foreach (Transform child in rankCardArea.transform) {
-      GameObject.Destroy(child.gameObject);
-    }
-
-    playArea.GetComponent<CardArea>().cards = new List<Card>();
-    Hand.GetComponent<CardArea>().cards = new List<Card>();
-
-    loadCards(_players[playerId].hand, Hand);
-    loadCards(_players[playerId].inPlay, playArea);
-    GameObject cardUI = Instantiate(RankCard);
-
-    // Get the rank asset.
-    string rankAsset = getRankAsset(_players[playerId].rank);
-
-    // Set the rank asset.
-    Sprite rankCard = Resources.Load<Sprite>(rankAsset);
-    cardUI.gameObject.GetComponent<Image>().sprite = rankCard;
-    cardUI.transform.SetParent(rankCardArea.transform);
-
-  }
-
-  // Load Cards
-  public List<Card> InitHand(){
-    List<Card> currCard = new List<Card>();
-
-    for(int i = 0 ; i < 12 ; i ++){
-      currCard.Add( _adventureDeck.Draw() );
-    }
-    return(currCard);
-  }
-
+  //Wake
   void Awake(){
     if(!_instance){
       _instance = this;
@@ -142,92 +89,24 @@ public class NetworkGameManager : NetworkBehaviour
     _storyDeck = new Deck("Story");
   }
 
-  // Load the cards up.
-	public void loadCards(List<Card> cards, GameObject area){
-		Card currCard;
+  //Give Players Their Card Hands
+  public List<Card> InitHand(){
+    List<Card> newHand = new List<Card>();
 
-		// Create Card GameObject's.
-		for(int i = 0 ; i < cards.Count; i++){
-			currCard = cards[i];
-
-			area.GetComponent<CardArea>().addCard(currCard);
-			GameObject CardUI = null;
-
-			// TODO: Clean this up.
-			if (currCard.GetType () == typeof(WeaponCard)) {
-				WeaponCard currWeapon = (WeaponCard)currCard;
-				CardUI = Instantiate (WeaponCard);
-				CardUI.GetComponent<WeaponCard>().name =  currWeapon.name;
-				CardUI.GetComponent<WeaponCard>().asset = currWeapon.asset;
-				CardUI.GetComponent<WeaponCard>().power = currWeapon.power;
-			} else if (currCard.GetType () == typeof(FoeCard)) {
-				FoeCard currFoe = (FoeCard)currCard;
-				CardUI = Instantiate (FoeCard);
-				CardUI.GetComponent<FoeCard>().name    = currFoe.name;
-				CardUI.GetComponent<FoeCard>().type    = currFoe.type;
-				CardUI.GetComponent<FoeCard>().loPower = currFoe.loPower;
-				CardUI.GetComponent<FoeCard>().hiPower = currFoe.hiPower;
-				CardUI.GetComponent<FoeCard>().special = currFoe.special;
-				CardUI.GetComponent<FoeCard>().asset   = currFoe.asset;
-			} else if (currCard.GetType () == typeof(AllyCard)) {
-				AllyCard currAlly = (AllyCard)currCard;
-				CardUI = Instantiate (AllyCard);
-				CardUI.GetComponent<AllyCard>().name    = currAlly.name;
-				CardUI.GetComponent<AllyCard>().asset   = currAlly.asset;
-				CardUI.GetComponent<AllyCard>().special = currAlly.special;
-				CardUI.GetComponent<AllyCard>().power   = currAlly.power;
-				CardUI.GetComponent<AllyCard>().bid     = currAlly.bid;
-				CardUI.GetComponent<AllyCard>().bonusPower = currAlly.bonusPower;
-				CardUI.GetComponent<AllyCard>().bonusBid  =  currAlly.bonusBid;
-				CardUI.GetComponent<AllyCard>().questCondition = currAlly.questCondition;
-				CardUI.GetComponent<AllyCard>().allyCondition  = currAlly.allyCondition;
-			} else if(currCard.GetType () == typeof(AmourCard)) {
-				AmourCard currAmour = (AmourCard)currCard;
-				CardUI = Instantiate (AmourCard);
-				CardUI.GetComponent<AmourCard>().name = currAmour.name;
-				CardUI.GetComponent<AmourCard>().asset = currAmour.asset;
-				CardUI.GetComponent<AmourCard>().power = currAmour.power;
-				CardUI.GetComponent<AmourCard>().bid = currAmour.bid;
-			} else if(currCard.GetType () == typeof(TestCard)){
-				TestCard currTest = (TestCard)currCard;
-				CardUI = Instantiate (TestCard);
-				CardUI.GetComponent<TestCard>().name = currTest.name;
-				CardUI.GetComponent<TestCard>().asset = currTest.asset;
-				CardUI.GetComponent<TestCard>().minBids = currTest.minBids;
-			}
-
-			// Load the card sprite.
-			Sprite card = Resources.Load<Sprite>(currCard.asset);
-			CardUI.gameObject.GetComponent<Image>().sprite = card;
-			CardUI.transform.SetParent(area.transform);
-
-			// Set the cards obj to it's UI.
-			currCard.obj = CardUI;
-		}
-	}
-
-  // Get the rank asset for a specific rank.
-	public string getRankAsset(int rank){
-		if(rank == 0){
-			return "card_image/rank/rankCard1";
-		} else if(rank == 1) {
-			return "card_image/rank/rankCard2";
-		} else if(rank == 2) {
-			return "card_image/rank/rankCard3";
-		}
-		return null;
-	}
+    for(int i = 0 ; i < 12 ; i ++){
+      newHand.Add(_adventureDeck.Draw());
+    }
+    return newHand;
+  }
 
 
+  //Start Runs After PLayers Join
   void Start(){
 
   }
 
   void Update(){
-    if(_players.Count > 1 && loaded == false){
-      loadPlayer(turnId);
-      loaded = true;
-    }
+
   }
 
 
--- Game/QUEST/Assets/script/PlayerObject.cs
+++ Game/QUEST/Assets/script/PlayerObject.cs
@@ -1,18 +1,157 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class PlayerObject : NetworkBehaviour {
-  Player thisPlayer;
-  void Update(){
 
-  }
+  public Player thisPlayer;
+
+  //Card Types
+  public GameObject Card;
+  public GameObject WeaponCard;
+  public GameObject FoeCard;
+  public GameObject AllyCard;
+  public GameObject QuestCard;
+  public GameObject AmourCard;
+  public GameObject TestCard;
+  public GameObject EventCard;
+  public GameObject RankCard;
+  public GameObject TournamentCard;
+
+  //Hand
+  public GameObject Hand;
+
+  // Text fields.
+  public GameObject playerIdTxt;
+  public GameObject shieldCounterTxt;
+
+
+  //PlayArea
+  public GameObject PlayArea;
+
+  public GameObject RankCardArea;
+
+
   void Awake(){
     //Debug.Log("Player Load");
+
     int id = NetworkGameManager.NetworkManager.GetCurrentNewId();
-    //Debug.Log(id);
     thisPlayer = new Player(id);
     thisPlayer.hand = NetworkGameManager.NetworkManager.InitHand();
     NetworkGameManager.NetworkManager._players.Add(thisPlayer);
+    loadHand();
   }
+
+
+  // Load the players hand onto the UI.
+  void loadHand(){
+    playerIdTxt.GetComponent<UnityEngine.UI.Text>().text = "Player ID: " + (thisPlayer.playerId);
+
+    // Get current players shield.
+    shieldCounterTxt.GetComponent<UnityEngine.UI.Text>().text = "# Shield: "+ (thisPlayer.shieldCounter);
+
+    foreach (Transform child in RankCardArea.transform) {
+      GameObject.Destroy(child.gameObject);
+    }
+
+    PlayArea.GetComponent<CardArea>().cards = new List<Card>();
+    Hand.GetComponent<CardArea>().cards = new List<Card>();
+
+    loadCards(thisPlayer.hand, Hand);
+    loadCards(thisPlayer.inPlay, PlayArea);
+    GameObject cardUI = Instantiate(RankCard);
+
+    // Get the rank asset.
+    string rankAsset = getRankAsset(thisPlayer.rank);
+
+    // Set the rank asset.
+    Sprite rankCard = Resources.Load<Sprite>(rankAsset);
+    cardUI.gameObject.GetComponent<Image>().sprite = rankCard;
+    cardUI.transform.SetParent(RankCardArea.transform);
+  }
+
+  // Load the cards up.
+	public void loadCards(List<Card> cards, GameObject area){
+		Card currCard;
+
+		// Create Card GameObject's.
+		for(int i = 0 ; i < cards.Count; i++){
+			currCard = cards[i];
+
+			area.GetComponent<CardArea>().addCard(currCard);
+			GameObject CardUI = null;
+
+			// TODO: Clean this up.
+			if (currCard.GetType () == typeof(WeaponCard)) {
+				WeaponCard currWeapon = (WeaponCard)currCard;
+				CardUI = Instantiate (WeaponCard);
+				CardUI.GetComponent<WeaponCard>().name =  currWeapon.name;
+				CardUI.GetComponent<WeaponCard>().asset = currWeapon.asset;
+				CardUI.GetComponent<WeaponCard>().power = currWeapon.power;
+			} else if (currCard.GetType () == typeof(FoeCard)) {
+				FoeCard currFoe = (FoeCard)currCard;
+				CardUI = Instantiate (FoeCard);
+				CardUI.GetComponent<FoeCard>().name    = currFoe.name;
+				CardUI.GetComponent<FoeCard>().type    = currFoe.type;
+				CardUI.GetComponent<FoeCard>().loPower = currFoe.loPower;
+				CardUI.GetComponent<FoeCard>().hiPower = currFoe.hiPower;
+				CardUI.GetComponent<FoeCard>().special = currFoe.special;
+				CardUI.GetComponent<FoeCard>().asset   = currFoe.asset;
+			} else if (currCard.GetType () == typeof(AllyCard)) {
+				AllyCard currAlly = (AllyCard)currCard;
+				CardUI = Instantiate (AllyCard);
+				CardUI.GetComponent<AllyCard>().name    = currAlly.name;
+				CardUI.GetComponent<AllyCard>().asset   = currAlly.asset;
+				CardUI.GetComponent<AllyCard>().special = currAlly.special;
+				CardUI.GetComponent<AllyCard>().power   = currAlly.power;
+				CardUI.GetComponent<AllyCard>().bid     = currAlly.bid;
+				CardUI.GetComponent<AllyCard>().bonusPower = currAlly.bonusPower;
+				CardUI.GetComponent<AllyCard>().bonusBid  =  currAlly.bonusBid;
+				CardUI.GetComponent<AllyCard>().questCondition = currAlly.questCondition;
+				CardUI.GetComponent<AllyCard>().allyCondition  = currAlly.allyCondition;
+			} else if(currCard.GetType () == typeof(AmourCard)) {
+				AmourCard currAmour = (AmourCard)currCard;
+				CardUI = Instantiate (AmourCard);
+				CardUI.GetComponent<AmourCard>().name = currAmour.name;
+				CardUI.GetComponent<AmourCard>().asset = currAmour.asset;
+				CardUI.GetComponent<AmourCard>().power = currAmour.power;
+				CardUI.GetComponent<AmourCard>().bid = currAmour.bid;
+			} else if(currCard.GetType () == typeof(TestCard)){
+				TestCard currTest = (TestCard)currCard;
+				CardUI = Instantiate (TestCard);
+				CardUI.GetComponent<TestCard>().name = currTest.name;
+				CardUI.GetComponent<TestCard>().asset = currTest.asset;
+				CardUI.GetComponent<TestCard>().minBids = currTest.minBids;
+			}
+
+			// Load the card sprite.
+			Sprite card = Resources.Load<Sprite>(currCard.asset);
+			CardUI.gameObject.GetComponent<Image>().sprite = card;
+			CardUI.transform.SetParent(area.transform);
+
+			// Set the cards obj to it's UI.
+			currCard.obj = CardUI;
+		}
+	}
+
+
+  // Get the rank asset for a specific rank.
+	public string getRankAsset(int rank){
+		if(rank == 0){
+			return "card_image/rank/rankCard1";
+		} else if(rank == 1) {
+			return "card_image/rank/rankCard2";
+		} else if(rank == 2) {
+			return "card_image/rank/rankCard3";
+		}
+		return null;
+	}
+
+
+
+
+
 }
